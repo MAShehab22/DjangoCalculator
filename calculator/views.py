bb8e924c1188367941d282ea83f2bdcb43f4d67d
MAShehab22 <azzouez@gmail.com>
2024-06-22 11:45:58 +0300

    AllStages

---
+++ calculator/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    if request.method == 'POST':
+        num1 = float(request.POST.get('num1'))
+        num2 = float(request.POST.get('num2'))
+        operation = request.POST.get('operation')
+        result = 0
+
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            result = num1 / num2
+
+        return render(request, 'index.html', {'result': result})
+
+    return render(request, 'index.html')
